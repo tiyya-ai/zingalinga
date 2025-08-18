2c537bc20437c9a3634103fd46f3caf2e80bc12f
User <user@example.com>
2025-08-18 23:46:42 +0100

    fix bundel issuseue

---
+++ src/components/ModernAdminDashboardBundles.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const renderAddBundle = () => {
+  return <div>Add Bundle Component</div>;
+};
+
+export const renderAllBundles = () => {
+  return <div>All Bundles Component</div>;
+};
